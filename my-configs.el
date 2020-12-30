974a2fb39d19bf07a2a92631d9fe24bb515d28dd
Vitor Mello <vitorml34@gmail.com>
2020-12-30 15:15:03 -0300

    updated comments

--- my-configs.el
+++ my-configs.el
@@ -1,24 +1,18 @@
 
 
+;; Setting theme
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(tango-dark)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(custom-set-faces)
 
 ;; Replaces selected region when typing-pasting
 (delete-selection-mode 1)
 
-;; Set preferable initial modes
+;; Hide bars
 (menu-bar-mode -1)
 (tool-bar-mode -1)
+
+;; Show matching parenthesis
 (show-paren-mode 1)
 
 ;; My functions for scrolling up and down and keeping the cursor position
@@ -36,6 +30,7 @@
 (global-set-key (kbd "C-<up>") 'my-scroll-down-and-center)
 (global-set-key (kbd "C-<down>") 'my-scroll-up-and-center)
 
+;; Binding comment-line to preferred keys. Also don't skip current line and go to 1st position
 (defun my-comment-line ()
   (interactive)
   (comment-line nil)
@@ -43,5 +38,6 @@
   (move-beginning-of-line nil))
 (global-set-key (kbd "C-q") 'my-comment-line)
 
+;; When splitting window automatically go to new one
 (global-set-key "\C-x2" (lambda () (interactive)(split-window-vertically) (other-window 1)))
 (global-set-key "\C-x3" (lambda () (interactive)(split-window-horizontally) (other-window 1)))
